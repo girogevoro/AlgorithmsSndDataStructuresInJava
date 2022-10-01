7c6af2615e949e607fd9c53e5f391c3691503668
egorovin <egorovin@mail.ru>
2022-10-02 02:13:14 +0300

    lesson 6

---
+++ src/main/java/Tree.java
@@ -0,0 +1,17 @@
+import java.util.function.Consumer;
+
+public interface Tree<T> {
+
+    void add(T item);
+
+    boolean contains(T item);
+
+    void remove(T item);
+
+    void dfs(Consumer<T> visitor);
+
+    void bfs(Consumer<T> visitor);
+
+    boolean isBalanced();
+
+}
